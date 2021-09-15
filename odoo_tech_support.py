67d6bd9bb4ac8035f6117e93760681e16019f81f
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-09-15 21:15:55 +0000

    Problem 2 done

--- odoo_tech_support.py
+++ odoo_tech_support.py
@@ -1,4 +1,4 @@
-import math
+import math, random
 
 # Problem 1:
 # Given an amount in dollars and a tax percentage. Return an array with the tax amount in cents.
@@ -32,5 +32,39 @@ print(get_tax_in_cents(10, .1)) # OUTPUT [100]
 print(get_tax_in_cents(546, .5)) # OUTPUT [27300]
 
 # Problem 2:
-# Given the deck ['A',1,2,3,4,5,6,7,8,9,10,'J','K','Q'], write a function that shuffles the deck using the randint function. 
-# The random module includes a basic function randint(a, b) that returns a uniformly random integer from a to b (including both endpoints). 
+# Given the deck ['A',1,2,3,4,5,6,7,8,9,10,'J','K','Q'], write a function that 
+# shuffles the deck using the randint function. 
+# The random module includes a basic function randint(a, b) that returns a 
+# uniformly random integer from a to b (including both endpoints). 
+# Example: 
+# Input deck = ['A',1,2,3,4,5,6,7,8,9,10,'J','K','Q'].
+# Output = ['Q','K',2,3,4,5,6,7,8,9,10,1,'A','J'] or can be in any shuffled order
+
+# pseudocode
+# get the len of deck
+# make a counter
+# while i is less than len
+# randint a num of len start end
+# pop that index deck into new list
+# increment counter
+# if item is already in new list, continue
+#
+
+
+def shuffle_deck(deck):
+
+    length_of_deck = len(deck)
+    i = 0
+    shuffled_deck = []
+    while i < length_of_deck:
+        rand_int = random.randint(0, int(length_of_deck))
+        if deck[rand_int-1] not in shuffled_deck:
+
+            shuffled_deck.append(deck[rand_int-1])
+            i += 1
+        print(rand_int)
+    return shuffled_deck
+    
+
+
+print(shuffle_deck(['A',1,2,3,4,5,6,7,8,9,10,'J','K','Q']))
